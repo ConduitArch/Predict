fa3b1746c5930fd9bd5fc22980bbbacee993013d
Uri Agassi <uri.agassi@perion.com>
2014-04-30 16:29:22 +0300

    refactor

--- src/Predict/Predict.java
+++ src/Predict/Predict.java
@@ -5,11 +5,16 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 
 public class Predict {
+
+	private static final long GROUP_IDENTIFIER = 0L;
 
 	public static void main(String[] args) throws IOException {
         if (args.length < 3)
@@ -22,19 +27,16 @@ public class Predict {
         String modelFile = args[1];
         String resultFile = args[2];
 
-        boolean hasLabel = true;
-        if (args.length == 4) hasLabel =  Boolean.parseBoolean(args[3]);
+//        boolean hasLabel = true;
+//        if (args.length == 4) hasLabel =  Boolean.parseBoolean(args[3]);
 
-        LoadModelNew(modelFile);
-        PredictNew(testFile, resultFile,hasLabel);		
+        Map<Long, Double> model = loadModel(modelFile);
+        predictAccuracy(testFile, resultFile, model);		
 	}	
 
-	static Map<Long,Double> modelFeaturesWeightsNew = null;
-    private static void LoadModelNew(String modelFile)
+    private static Map<Long,Double> loadModel(String modelFile) throws IOException
     {
-		long start = System.currentTimeMillis();
-        try {
-    		BufferedReader r = new BufferedReader(new FileReader(modelFile));   
+        try (BufferedReader r = new BufferedReader(new FileReader(modelFile))) {   
 			r.readLine(); // solverType
 			r.readLine(); // classNumber
 			r.readLine(); // labels
@@ -42,90 +44,135 @@ public class Predict {
 			r.readLine(); // bias
 			r.readLine();
 			
-			modelFeaturesWeightsNew = new TreeMap<Long,Double>();
+			Map<Long,Double> modelFeatureWeights = new TreeMap<Long,Double>();
 			String line;
 			while ((line = r.readLine()) != null)
 			{
 				String[] fields = line.split("\t");
 				long idx = Long.parseLong(fields[0]);
 				double weight = Double.parseDouble(fields[1]);
-				modelFeaturesWeightsNew.put(idx,weight);
+				modelFeatureWeights.put(idx,weight);
 				}
-			r.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			return modelFeatureWeights;
 		}
-        long end = System.currentTimeMillis();
-        System.out.println("Loading model took " + (end-start) + " milliseconds.");
+    }
+    
+    private static <T> double multiplyVectors(Map<T, Double> shortVector, Map<T, Double> longVector) {
+    	if (shortVector.size() > longVector.size()) return multiplyVectors(longVector, shortVector);
+    	
+    	double sum = 0;
+    	
+    	for (T feature : shortVector.keySet()) {
+    		if (longVector.containsKey(feature)) {
+    			sum += shortVector.get(feature) * longVector.get(feature);
+    		}
+    	}
+    	
+    	return sum;
+    }
+    
+    private static List<Map<Long, Double>> readFeatureVectors(String testFile) throws IOException {
+    	List<Map<Long, Double>> result = new LinkedList<>();
+    	try (BufferedReader r = new BufferedReader(new FileReader(testFile))) {
+    		String line;
+			while ((line = r.readLine()) != null) {
+    		    Map<Long, Double> vector = new HashMap<>();
+    		    String[] values = line.split(" ");
+    		    for (String value : values) {
+    		    	if (value == values[0]) {
+    		    		vector.put(GROUP_IDENTIFIER, Double.parseDouble(value));
+    		    		continue;
+    		    	}
+    		    	String[] fields = value.split(":");
+    		    	vector.put(Long.parseLong(fields[0]), Double.parseDouble(fields[1]));
+    		    }
+    		    result.add(vector);
+    		}
+    	}
+    	return result;
+    }
+    
+    public static int[][] mapPrediction(List<Map<Long, Double>> featureVectors, Map<Long, Double> model) {
+    	int[][] results = new int[2][2];
+    	for (Map<Long, Double> vector : featureVectors) {
+    		double score = multiplyVectors(vector, model);
+    		results[score > 0 ? 1 : 0][vector.get(GROUP_IDENTIFIER) > 0 ? 1 : 0] += 1;
+    	}
+    	return results;
+    }
+    
+    public static double getAccuracy(int[][] prediction) {
+    	return (prediction[0][0] + prediction[1][1]) / (prediction[0][0] + prediction[1][1] + prediction[0][1] + prediction[1][0]);
+    }
+    
+    public static double getSpecificity(int[][] prediction) {
+    	return prediction[0][0] / (prediction[0][0] + prediction[1][0]);
+    }
+    
+    public static double getSensitivity(int[][] prediction) {
+    	return prediction[1][1] / (prediction[1][1] + prediction[0][1]);
+    }
+    
+    public static double getAccuracy(String testFile, String modelFile) throws IOException {
+    	return getAccuracy(mapPrediction(readFeatureVectors(testFile), loadModel(modelFile)));
     }
 
-    private static void PredictNew(String testFile, String resultFile, boolean hasLabel) throws IOException
+    private static double predictAccuracy(String testFile, String resultFile, Map<Long, Double> model) throws IOException
     {
-    	long start = System.currentTimeMillis();
-    	BufferedReader r = new BufferedReader(new FileReader(testFile));   
-    	BufferedWriter  sw = new BufferedWriter (new FileWriter(resultFile));
-        int instanceAmount = 0;
+    	try (BufferedReader r = new BufferedReader(new FileReader(testFile));   
+    	BufferedWriter  sw = new BufferedWriter (new FileWriter(resultFile))) {
+    		int truePositives  = 0;  // positive instances that were classified as positive
+	        int falseNegatives = 0;  // positive instances that were classified as positive
+	        int trueNegatives  = 0;  // negative instances that were classified as negative
+	        int falsePositives = 0;  // negative instances that were classified as negative
+	
+	
+	        String line;
+			while ((line = r.readLine()) != null)
+	        {
+	            String[] values = line.split(" ");
+	            String label = "N/A";
+	            label = values[0];
+	
+	            double sum = 0.0;
+	            for (String value : values)
+	            {
+	                if (value.trim().length() == 0) continue; // The last line might be empty
+	                String[] fields = value.split(":");
+	                long featureIndex = Long.parseLong(fields[0]);
+	                if (model.containsKey(featureIndex))
+	                {
+	                    double instanceFeatureValue = Double.parseDouble(fields[1]);
+	                    sum += instanceFeatureValue * model.get(featureIndex);
+	                }
+	            }
+	            if (label.equals("1") && sum > 0.0) ++truePositives;
+	            if (label.equals("1") && sum <= 0.0) ++falseNegatives;
+	            if (!label.equals("1") && sum > 0.0) ++falsePositives;
+	            if (!label.equals("1") && sum <= 0.0) ++trueNegatives;
+	
+	            sw.append(label + "\t" + sum);
+	            sw.newLine();
+	        }
 
-        int true_positives  = 0;  // positive intances that were classified as positive
-        int false_negatives = 0;  // positive intances that were classified as positive
-        int true_negatives  = 0;  // negative intances that were classified as negative
-        int false_positives = 0;  // negative intances that were classified as negative
-
-
-        String line;
-		while ((line = r.readLine()) != null)
-        {
-            ++instanceAmount;
-            String[] values = line.split(" ");
-            String label = "N/A";
-            if (hasLabel) label = values[0];
-
-            double sum = 0.0;
-            int i = 0;
-            if (hasLabel) i = 1;
-            for (; i < values.length; i++)
-            {
-            	String value = values[i];
-                if (value.trim().length() == 0) continue; // The last line might be empty
-                String[] fields = value.split(":");
-                long featureIndex = Long.parseLong(fields[0]);
-                if (modelFeaturesWeightsNew.containsKey(featureIndex))
-                {
-                    double instanceFeatureValue = Double.parseDouble(fields[1]);
-                    sum += instanceFeatureValue * modelFeaturesWeightsNew.get(featureIndex);
-                }
-            }
-            if (label.equals("1") && sum > 0.0) ++true_positives;
-            if (label.equals("1") && sum <= 0.0) ++false_negatives;
-            if (!label.equals("1") && sum > 0.0) ++false_positives;
-            if (!label.equals("1") && sum <= 0.0) ++true_negatives;
-
-            sw.append(label + "\t" + sum);
-            sw.newLine();
-        }
-        r.close();
-        sw.close();
-        long end = System.currentTimeMillis();
-        System.out.println("Prediction of " + instanceAmount + " took " + (end-start) + " milliseconds. (Average of " + (double)((end-start) / instanceAmount) + " milliseconds per instance).");
-        if (hasLabel)
-        {
-        	Double Precision = (double)true_positives / (true_positives + false_positives);
-            Double Negative_predictive_value = (double)true_negatives / (true_negatives + false_negatives);
-            Double Sensitivity = (double)true_positives / (true_positives + false_negatives);
-            Double Spcecifity = (double)true_negatives / (true_negatives + false_positives);
-            Double Accuracy = (double)(true_positives + true_negatives) / (true_positives + false_negatives + true_negatives + false_positives);
-            Double Recall = Sensitivity;
-            // Double F1 = 2 * Precision * Recall / (Precision + Recall);
-            Double accPer = Accuracy*100;
-            System.out.println("Accuracy = "+accPer+"% (" +(true_positives+true_negatives)+"/"+(true_positives+false_negatives+true_negatives + false_positives)+")");
-            System.out.println("accuracy="+Accuracy +
-            		           ", Precision="+Precision+
-            		           ",Recall="+Recall+
-            		           ",NegativePredictiveValue="+Negative_predictive_value+
-            		           ",Sensitivity="+Sensitivity+
-            		           ",Specifity="+Spcecifity);
-        }
+//	        double precision = truePositives / (truePositives + falsePositives);
+//			double negativePredictiveValue = trueNegatives / (trueNegatives + falseNegatives);
+//			double sensitivity = truePositives / (truePositives + falseNegatives);
+//			double specificity = trueNegatives / (trueNegatives + falsePositives);
+//			double accuracy = (truePositives + trueNegatives) / (truePositives + falseNegatives + trueNegatives + falsePositives);
+//			double recall = sensitivity;
+			// Double F1 = 2 * Precision * Recall / (Precision + Recall);
+//			double accPer = accuracy*100;
+//			System.out.println("Accuracy = "+accPer+"% (" +(truePositives+trueNegatives)+"/"+(truePositives+falseNegatives+trueNegatives + falsePositives)+")");
+//			System.out.println("accuracy="+accuracy +
+//					           ", Precision="+precision+
+//					           ",Recall="+recall+
+//					           ",NegativePredictiveValue="+negativePredictiveValue+
+//					           ",Sensitivity="+sensitivity+
+//					           ",Specifity="+specificity);
+			return (truePositives + trueNegatives) / (truePositives + falseNegatives + trueNegatives + falsePositives);
+		}
+    	
     }
 
 }
